9789f95bda19263cea3c8d69109dc95f39f29164
Douglas Bates <dmbates@gmail.com>
2013-07-16 14:40:59 -0500

    Add cdf, quantile, ccdf and cquantile in Julia.

--- src/univariate/normal.jl
+++ src/univariate/normal.jl
@@ -11,7 +11,7 @@ Normal() = Normal(0.0, 1.0)
 
 @_jl_dist_2p Normal norm
 
-@Base.math_const twoπ   6.283185307179586  big(2.)*π
+@Base.math_const √2     1.4142135623730951 sqrt(big(2.))
 @Base.math_const log2π  1.8378770664093456 log(big(2.)*π)
 @Base.math_const √2π    2.5066282746310007 sqrt(big(2.)*π)
 
@@ -19,14 +19,31 @@ const Gaussian = Normal
 
 begin
     zval(d::Normal, x::Real) = (x - d.μ)/d.σ
+    xval(d::Normal, z::Real) = d.μ + d.σ * z
+    
     φ{T<:FloatingPoint}(z::T) = exp(-0.5*z*z)/√2π
-    logφ{T<:FloatingPoint}(z::T) = -0.5*(z*z + log2π)
-
     pdf(d::Normal, x::FloatingPoint) = φ(zval(d,x))/d.σ
-    pdf(d::Normal, x::Integer) = pdf(d, float64(x))
 
+    logφ{T<:FloatingPoint}(z::T) = -0.5*(z*z + log2π)
     logpdf(d::Normal, x::FloatingPoint) = logφ(zval(d,x)) - log(d.σ)
-    logpdf(d::Normal, x::Integer) = logpdf(d, float64(x))
+
+    Φ{T<:FloatingPoint}(z::T) = 0.5 + 0.5*erf(z/√2)
+    cdf(d::Normal, x::FloatingPoint) = Φ(zval(d,x))
+
+    Φc{T<:FloatingPoint}(z::T) = 0.5*erfc(z/√2)
+    ccdf(d::Normal, x::FloatingPoint) = Φc(zval(d,x))
+
+    Φinv{T<:FloatingPoint}(p::T) = √2 * erfinv(2p - 1)
+    quantile(d::Normal, p::FloatingPoint) = xval(d, Φinv(p))
+
+    Φcinv{T<:FloatingPoint}(p::T) = √2 * erfcinv(2p)
+    cquantile(d::Normal, p::FloatingPoint) = xval(d, Φcinv(p))
+end
+
+for f in [pdf, logpdf, ccdf, cdf, quantile]
+    quote
+        $f(d::Normal, x::Integer) = $f(d, float64(x))
+    end
 end
 
 entropy(d::Normal) = log2π + 0.5 + log(d.σ)
